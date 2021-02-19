d1a1240e6c17500c8a2a2993945a773680e9c96b
Robert Lundsten <robert@onify.co>
2021-02-19 14:42:32 +0100

    fix: files

--- resources/scripts/servicenow/auth_headers.js
+++
@@ -1,6 +0,0 @@
-const settings = environment.output.serviceNowSettings;
-const headers = {};
-const authToken = 'Basic ' + Buffer.from(settings._servicenow_username + ':' + settings._servicenow_password).toString('base64');
-headers.Authorization = authToken;
-
-next(null, headers);
